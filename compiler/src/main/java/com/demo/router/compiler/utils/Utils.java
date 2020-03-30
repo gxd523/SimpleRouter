58604a78d5bc5b3d11870ffe8325583ca852d9f7
gxd <guoxiaodong@haqu.com>
2020-03-30 17:40:50 +0800

    nothing

--- compiler/src/main/java/com/demo/router/compiler/utils/Utils.java
+++ compiler/src/main/java/com/demo/router/compiler/utils/Utils.java
@@ -1,10 +1,8 @@
 package com.demo.router.compiler.utils;
 
 import java.util.Collection;
-import java.util.Map;
 
 public class Utils {
-
     public static boolean isEmpty(CharSequence cs) {
         return cs == null || cs.length() == 0;
     }
@@ -12,9 +10,4 @@ public class Utils {
     public static boolean isEmpty(Collection<?> coll) {
         return coll == null || coll.isEmpty();
     }
-
-    public static boolean isEmpty(final Map<?, ?> map) {
-        return map == null || map.isEmpty();
-    }
-
 }
